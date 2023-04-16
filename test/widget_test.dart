8d2b0ac73b9feaf0972c1a401b011f78c5756f30
Toan Nguyen V <toan.ngvan94@gmail.com>
2023-04-16 16:02:39 +0700

    Create use case, repository

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:todo_list/main.dart';
+import 'package:todo_list/presentation/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
